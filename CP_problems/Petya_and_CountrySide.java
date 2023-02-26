7b2896b7685ed5e9615544f6df019a44257ba275
shafingit1234 <shafinmohammed315@gmail.com>
2023-02-26 22:16:24 +0530

    commiting codeforces code

---
+++ CP_problems/Petya_and_CountrySide.java
@@ -0,0 +1,46 @@
+package CP_problems;
+
+import java.util.Scanner;
+
+public class Petya_and_CountrySide {
+	public static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		int n = sc.nextInt();
+		int[] hts = new int[n];
+		for(int i=0 ; i<n ; i++) {
+			hts[i] = sc.nextInt();
+		}
+		int[] left = new int[n];
+		int[] right=new int[n];
+		left[0] = 0;
+		int end = n-1;
+		right[end] = end;
+		int temp = 0;
+		for(int i=1 ; i<n ; i++) {
+			if(hts[i] < hts[i-1]) {
+				temp = i;
+				left[i]= i;
+			}
+			else {
+				left[i] = temp;
+			}
+		}
+		temp = end;
+		for(int i=end -1; i>=0 ; i--) {
+			if(hts[i] >= hts[i+1]) {
+				right[i] = temp;
+			}
+			else {
+				temp = i;
+				right[i] = i;
+			}
+		}
+		//find the answer;
+		int ans = Integer.MIN_VALUE;
+		for(int i=0 ; i<n;i++) {
+			temp = Math.abs(left[i] - right[i]) + 1;
+			ans = Math.max(ans,  temp);
+		}
+		System.out.println(ans);
+	}
+}
---
+++ CP_problems/Vanya_and_fence.java
@@ -0,0 +1,27 @@
+package CP_problems;
+
+import java.util.Scanner;
+
+public class Vanya_and_fence {
+	public static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		long n, m, i, j, h;
+		n = sc.nextLong();
+		h = sc.nextLong();
+		i = 0;
+		long ans1 = 0;
+		long ans2 = 0;
+		while(i < n) {
+			m = sc.nextLong();
+			if(m <= h) {
+				ans1++;
+			}
+			else {
+				ans2++;
+			}
+			i++;
+		}
+		System.out.println(ans1 + ans2*2);
+		return ;
+	}
+}
