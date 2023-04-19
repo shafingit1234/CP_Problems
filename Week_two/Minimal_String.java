e6372ae38f198b76e3967c24aa0d3b5f157b732f
shafingit1234 <shafinmohammed315@gmail.com>
2023-04-19 22:53:29 +0530

    adding solutions

---
+++ Week_two/Minimal_String.java
@@ -0,0 +1,108 @@
+package Week_two;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+//import Java_CP_Template.Template_One.FastScanner;
+
+public class Minimal_String {
+	public static FastScanner sc = new FastScanner();
+	public static long mod = (long)(1e9 + 7);
+	public static Long modexp(Long a,Long b)
+    { Long ans=(long)1;
+        while(b>0)
+        {
+          if((b&1)!=0)// b%2!=0
+          {
+              ans=(ans*a)%mod;
+          }
+          a=(a*a)%mod;
+          b=b>>1;  
+        }
+        return ans;
+    }
+	public static void findAnswer() {
+		String str = sc.next();
+		//find the last occurence of all possible characters in the string.
+		int[] freq = new int[26];
+		for(int i=0 ; i<str.length() ; i++) {
+			freq[str.charAt(i) - 'a'] = i;
+		}
+		Stack<Integer> st = new Stack<>();
+		boolean visited[] = new boolean[str.length()];
+		for(int i=0 ; i<26; i++) {
+			while(!st.isEmpty() && str.charAt(st.peek()) <= 'a' + i) {
+//				char ch = (char) ('a' + st.pop());
+//				System.out.print(ch);
+				System.out.print(str.charAt(st.pop()));
+			}
+			int frequency = freq[i];
+			for(int j=0 ; j<=frequency ; j++) {
+				if(visited[j] == true) continue;
+				else {
+					if(str.charAt(j) == (char)('a' + i)) {
+						System.out.print(str.charAt(j));
+					}
+					else {
+						st.push(j);
+					}
+				}
+				visited[j] = true;
+			}
+		}
+		while(!st.isEmpty()) {
+			System.out.print(str.charAt(st.pop()));
+		}
+		System.out.println();
+	}
+	public static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+ 
+        String next() {
+            while (!st.hasMoreTokens()) try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+ 
+    }
+	public static final Random random = new Random();
+    public static void ruffleSort(int[] a) {
+        int n = a.length;//shuffle, then sort
+        for (int i = 0; i < n; i++) {
+            int oi = random.nextInt(n), temp = a[oi];
+            a[oi] = a[i];
+            a[i] = temp;
+        }
+        Arrays.sort(a);
+    }
+	public static void main(String[] args) {
+//		int tc = sc.nextInt();
+		int tc = 1;
+		while(tc-- > 0) {
+			findAnswer();
+		}
+	}
+}
