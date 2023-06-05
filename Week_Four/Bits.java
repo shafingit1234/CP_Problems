4c279df09be511c46da8cd348282631f4f904ab5
shafingit1234 <shafinmohammed315@gmail.com>
2023-06-05 20:15:46 +0530

    adding solutions

---
+++ Week_Four/Bits.java
@@ -0,0 +1,102 @@
+package Week_Four;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+import Java_CP_Template.Template_One.FastScanner;
+
+public class Bits {
+	public static FastScanner sc = new FastScanner();
+	public static long mod = (long)(1e9 + 7);
+	public static Long modexp(Long a,Long b)
+    { Long ans=(long)1;
+        while(b>0)
+        {
+          if((b&1)!=0)// b%2!=0
+          {
+              ans=(ans*a)%mod;
+          }
+          a=(a*a)%mod;
+          b=b>>1;  
+        }
+        return ans;
+    }
+	public static int findBit(long r) {
+		int k = 32;
+		while(k >= 0) {
+			if(((1<<k)&r) != 0) {
+				break;
+			}
+			
+			k--;
+		}
+		return k;
+	}
+	public static void findAnswer() {
+		int q = sc.nextInt();
+		while(q > 0) {
+			q--;
+			long l = sc.nextLong();
+			long r = sc.nextLong();
+//			int loc = findBit(r);
+//			long temp = (1<<loc) - 1;
+//			temp = Math.max(loc,temp);
+////			System.out.println(loc + " " + temp);
+//			System.out.println(temp);
+			while((l|(l+1)) <= r) {
+				l |= l+1;
+			}
+			System.out.println(l);
+		}
+		
+	}
+	public static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+ 
+        String next() {
+            while (!st.hasMoreTokens()) try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+ 
+    }
+	public static final Random random = new Random();
+    public static void ruffleSort(int[] a) {
+        int n = a.length;//shuffle, then sort
+        for (int i = 0; i < n; i++) {
+            int oi = random.nextInt(n), temp = a[oi];
+            a[oi] = a[i];
+            a[i] = temp;
+        }
+        Arrays.sort(a);
+    }
+	public static void main(String[] args) {
+//		int tc = sc.nextInt();
+		int tc = 1;
+		while(tc-- > 0) {
+			findAnswer();
+		}
+	}
+}
---
+++ Week_Four/Diwan_and_Bitwise_Operations.java
@@ -0,0 +1,93 @@
+package Week_Four;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+import Java_CP_Template.Template_One.FastScanner;
+
+public class Diwan_and_Bitwise_Operations {
+	public static FastScanner sc = new FastScanner();
+	public static long mod = (long)(1e9 + 7);
+	public static Long modexp(Long a,Long b)
+    { Long ans=(long)1;
+        while(b>0)
+        {
+          if((b&1)!=0)// b%2!=0
+          {
+              ans=(ans*a)%mod;
+          }
+          a=(a*a)%mod;
+          b=b>>1;  
+        }
+        return ans;
+    }
+	public static void findAnswer() {
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		long xr = 0;
+		// 7 * 16 = 112
+		while(m > 0) {
+			m--;
+			int l = sc.nextInt();
+			int r = sc.nextInt();
+			long val = sc.nextLong();
+			xr = xr|val;
+		}
+		xr = xr%mod;
+		for(int i=0 ; i<n-1 ; i++) {
+			xr = xr*2;
+			xr = xr%mod;
+		}
+		System.out.println(xr);
+	}
+	public static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+ 
+        String next() {
+            while (!st.hasMoreTokens()) try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+ 
+    }
+	public static final Random random = new Random();
+    public static void ruffleSort(int[] a) {
+        int n = a.length;//shuffle, then sort
+        for (int i = 0; i < n; i++) {
+            int oi = random.nextInt(n), temp = a[oi];
+            a[oi] = a[i];
+            a[i] = temp;
+        }
+        Arrays.sort(a);
+    }
+	public static void main(String[] args) {
+//		int tc = sc.nextInt();
+		int tc = sc.nextInt();
+		while(tc-- > 0) {
+			findAnswer();
+		}
+	}
+}
