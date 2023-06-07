34c1b4d9925f28d49cf90a76a5baffdee6ec2868
shafingit1234 <shafinmohammed315@gmail.com>
2023-06-08 01:01:33 +0530

    adding solutions

---
+++ Week_Four/You_are_two_binary_strings.java
@@ -0,0 +1,95 @@
+package Week_Four;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+import Java_CP_Template.Template_One.FastScanner;
+
+public class You_are_two_binary_strings {
+	public static FastScanner sc = new FastScanner();
+	public static long mod = (long)(1e9 + 7);
+	public static Long modexp(Long a,Long b)
+    { Long ans=(long)1;
+        while(b>0)
+        {
+          if((b&1)!=0)// b%2!=0
+          {
+              ans=(ans*a)%mod;
+          }
+          a=(a*a)%mod;
+          b=b>>1;  
+        }
+        return ans;
+    }
+	public static void findAnswer() {
+		String s_one = sc.next();
+		String s_two = sc.next();
+		s_one = new StringBuilder(s_one).reverse().toString();
+		s_two = new StringBuilder(s_two).reverse().toString();
+		int pos_one = 0;
+		int pos_two = 0;
+		for(int i=0 ; i<s_two.length() ; i++) {
+			if(s_two.charAt(i) == '1') {
+				pos_two = i;
+				break;
+			}
+		}
+		for(int j=0 ; j<s_one.length() ; j++) {
+			if(s_one.charAt(j) == '1' && j >= pos_two) {
+				pos_one = j;
+				break;
+			}
+		}
+		int ans = pos_one - pos_two;
+		System.out.println(ans);
+	}
+	public static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+ 
+        String next() {
+            while (!st.hasMoreTokens()) try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+ 
+    }
+	public static final Random random = new Random();
+    public static void ruffleSort(int[] a) {
+        int n = a.length;//shuffle, then sort
+        for (int i = 0; i < n; i++) {
+            int oi = random.nextInt(n), temp = a[oi];
+            a[oi] = a[i];
+            a[i] = temp;
+        }
+        Arrays.sort(a);
+    }
+	public static void main(String[] args) {
+		int tc = sc.nextInt();
+//		int tc = 1;
+		while(tc-- > 0) {
+			findAnswer();
+		}
+	}
+}
