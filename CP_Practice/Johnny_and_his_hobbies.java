2539f1429b9f5569a448d3403e596a096a5ac3d5
shafingit1234 <shafinmohammed315@gmail.com>
2023-07-03 12:44:14 +0530

    adding solutions

---
+++ CP_Practice/Johnny_and_his_hobbies.java
@@ -0,0 +1,99 @@
+package CP_Practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+//import Java_CP_Template.Template_One.FastScanner;
+
+public class Johnny_and_his_hobbies {
+	public static FastScanner sc = new FastScanner();
+	public static long mod = (long)(1e9 + 7);
+	public static Long modexp(Long a,Long b)
+    { Long ans=(long)1;
+        while(b>0)
+        {
+          if((b&1)!=0)// b%2!=0
+          {
+              ans=(ans*a)%mod;
+          }
+          a=(a*a)%mod;
+          b=b>>1;  
+        }
+        return ans;
+    }
+	public static void findAnswer() {
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i=0 ; i<n ; i++) {
+			arr[i] = sc.nextInt();
+		}
+		HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+		for(int i=0 ; i<n ; i++) {
+			for(int j = 0 ; j<n ; j++) {
+				if(i == j) continue;
+				int ele = arr[i] ^ arr[j];
+				hm.put(ele, hm.getOrDefault(ele, 0) + 1);
+			}
+		}
+		int ans = Integer.MAX_VALUE;
+		for(int i : hm.keySet()) {
+			if(hm.get(i) == n) {
+				ans = Math.min(ans, i);
+			}
+		}
+		if(ans == Integer.MAX_VALUE) {
+			ans = -1;
+		}
+		System.out.println(ans);
+	}
+	public static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+ 
+        String next() {
+            while (!st.hasMoreTokens()) try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+ 
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+ 
+    }
+	public static final Random random = new Random();
+    public static void ruffleSort(int[] a) {
+        int n = a.length;//shuffle, then sort
+        for (int i = 0; i < n; i++) {
+            int oi = random.nextInt(n), temp = a[oi];
+            a[oi] = a[i];
+            a[i] = temp;
+        }
+        Arrays.sort(a);
+    }
+	public static void main(String[] args) {
+		int tc = sc.nextInt();
+//		int tc = 1;
+		while(tc-- > 0) {
+			findAnswer();
+		}
+	}
+}
