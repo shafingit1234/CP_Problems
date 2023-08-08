05ff39f063d6eae1e5bd8e48515c09ff07d8f077
shafingit1234 <shafinmohammed315@gmail.com>
2023-08-09 00:27:18 +0530

    adding solutions

--- CP_Practice/Ira_and_Flamenco.java
+++ CP_Practice/Ira_and_Flamenco.java
@@ -4,8 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Random;
 import java.util.StringTokenizer;
 
@@ -36,58 +38,104 @@ public class Ira_and_Flamenco {
 			a[i] = sc.nextLong();
 			hm.put(a[i] , hm.getOrDefault(a[i], (long) 0) + 1);
 		}
-		Arrays.sort(a);
+//		System.out.println(hm.size());
+		int n1 = hm.size();
+//		System.out.println(n1);
+		long arr[][] = new long[hm.size()][2];
+		int i = 0;
+		for(Map.Entry<Long, Long> e : hm.entrySet()) {
+			arr[i][0] = e.getKey();
+			arr[i++][1] = e.getValue();
+		}
+		//sort the array on the basis of the key.
+		Arrays.sort(arr, new Comparator<long[]>(){
+			public int compare(long[] o1, long [] o2) {
+				return (int)(o1[0] - o2[0]);
+			}
+		});
+		//create a prefix array storing the mulitiplication of frequencies of the elements in the array.
+		long[] prefix = new long[hm.size()+1];
+		Arrays.fill(prefix, 1l);
+		for(int j = 1 ; j<=arr.length; j++) {
+			prefix[j] = (prefix[j-1]*arr[j-1][1])%mod;
+		}
+		
+		
+		hm.clear();
 		HashSet<Long> hs =new HashSet<Long>();
 		int start = 0;
 		int end= 0;
 		long ans = 0;
-		while(end <= n) {
-			if(hs.size() == m) {
-				long mult = 1;
-				for(long val : hs) {
-					long b =hm.get(val);
-//					System.out.print(val + " " + b + "   ");
-					if(b == 1) {
-						continue;
-					}
-					mult = ((mult)*(b))%mod;
-				}
-//				System.out.println();
-				ans = (ans + mult)%mod;
-				//shift the window
-				hs.remove(a[start]);
+//		while(end <= n1) {
+////			System.out.println(end + " " + start);
+//			if(hs.size() == m) {
+//				//remove
+//				long mult = (prefix[end]/prefix[start]);
+//				ans = (ans + mult)%mod;
+//				hs.remove(arr[start][0]);
 //				start++;
-				long temp = a[start];
-				while(start < n && temp == a[start]) {
-					start++;
-				}
-				if(end < start) {
-					end = start;
-				}
-				continue;
-			}
-			if(end == n || start >= n) {
+//				if(end < start) {
+//					end = start;
+//				}
+//				continue;
+//			}
+//			if(end >= n1 || start >= n1) {
+//				break;
+//			}
+//			if(hs.size() < m && Math.abs(arr[start][0] - arr[end][0]) < m) {
+//				hs.add(arr[end][0]);
+//			}
+//			else if(hs.size() < m && Math.abs(arr[start][0] - arr[end][0]) >= m) {
+//				hs.remove(arr[start][0]);
+//				start++;
+//				if(end < start) {
+//					end = start;
+//				}
+//				continue;
+//			}
+//			end++;
+//		}
+		for(int k = 0 ; k<n1 ; k++) {
+			if(k + m -1 >= n1) {
 				break;
 			}
-			if(hs.size() < m && Math.abs(a[start] - a[end]) < m) {
-				hs.add(a[end]);
+			if(arr[k+m-1][0] - arr[k][0] == m-1) {
+				ans = (ans + (prefix[k + m])*modInverse(prefix[k], mod)%mod)%mod;
 			}
-			else if(hs.size() < m && Math.abs(a[start] - a[end]) >= m) {
-				hs.remove(a[start]);
-//				start++;
-				long temp = a[start];
-				while(start < n && temp == a[start]) {
-					start++;
-				}
-				if(end < start) {
-					end = start;
-				}
-				continue;
-			}
-			end++;
 		}
 		System.out.println(ans);
 	}
+	static long modInverse(long A, long M)
+    {
+        long m0 = M;
+        long y = 0, x = 1;
+ 
+        if (M == 1l)
+            return 0;
+ 
+        while (A > 1) {
+            // q is quotient
+            long q = A / M;
+ 
+            long t = M;
+ 
+            // m is remainder now, process
+            // same as Euclid's algo
+            M = A % M;
+            A = t;
+            t = y;
+ 
+            // Update x and y
+            y = x - q * y;
+            x = t;
+        }
+ 
+        // Make x positive
+        if (x < 0)
+            x += m0;
+ 
+        return x;
+    }
 	public static class FastScanner {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer("");
